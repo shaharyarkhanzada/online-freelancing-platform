4c709b4f23b36d01bbdd32e6cd8917480a93b684
shaharyarkhanzada <166238968+shaharyarkhanzada@users.noreply.github.com>
2024-04-06 21:37:16 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include_once('includes/config.php');?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Online job post</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+<?php include_once('includes/header.php') ?>
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/1.jpg" alt="..." /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light">WELCOME TO FREELANCING PLATFORM</h1>
+                    <p>Enter your job detail we will give you the best team for your project.</p>
+                    <a class="btn btn-primary" href="reporting.php">post job</a>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4 text-center">
+                <div class="card-body"><p class="text-white m-0"></p></div>
+            </div>
+            <!-- Content Row-->
+     
+        </div>
+        <!-- Footer-->
+<?php include_once('includes/footer.php') ?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
